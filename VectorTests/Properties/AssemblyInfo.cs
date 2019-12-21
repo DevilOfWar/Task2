94d1368044eea1bd842a47ed7c99f2e9f1dd6e27
Павел Свинтицкий <pasha.svinticki@gmail.com>
2019-12-21 09:29:39 +0300

    Added test to summ operator

---
+++ VectorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VectorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("VectorTests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4ecef3c3-e9ce-4ac8-91bd-8fafd9a2f962")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VectorTests/VectorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Task2.Tests
+{
+    [TestClass]
+    public class VectorTests
+    {
+        [TestMethod]
+        public void SummTest()
+        {
+            Vector firstVector = new Vector(10, -4, 3);
+            Vector secondVector = new Vector(4.2, 1.6, -4.35);
+            Vector resultVector = firstVector + secondVector;
+            Vector expectedResultVector = new Vector(14.2, -2.4, 3 - 4.35);
+            Assert.AreEqual(expectedResultVector.X, resultVector.X);
+            Assert.AreEqual(expectedResultVector.Y, resultVector.Y);
+            Assert.AreEqual(expectedResultVector.Z, resultVector.Z);
+            firstVector = new Vector(0, 0, 0);
+            resultVector = firstVector + secondVector;
+            expectedResultVector = new Vector(4.2, 1.6, -4.35);
+            Assert.AreEqual(expectedResultVector.X, resultVector.X);
+            Assert.AreEqual(expectedResultVector.Y, resultVector.Y);
+            Assert.AreEqual(expectedResultVector.Z, resultVector.Z);
+            firstVector = new Vector(3.5, -4, 17);
+            secondVector = new Vector(4.2, -0.6, -84);
+            resultVector = firstVector + secondVector;
+            expectedResultVector = new Vector(7.7, -4.6, -67);
+            Assert.AreEqual(expectedResultVector.X, resultVector.X);
+            Assert.AreEqual(expectedResultVector.Y, resultVector.Y);
+            Assert.AreEqual(expectedResultVector.Z, resultVector.Z);
+
+
+        }
+    }
+}
