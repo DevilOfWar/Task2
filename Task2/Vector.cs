96e0524e400f6dcf7ea2e34b80f4055d085a0340
Павел Свинтицкий <pasha.svinticki@gmail.com>
2019-12-21 08:34:54 +0300

    Create class Vector.

---
+++ Task2/Vector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class Vector
+    {
+        public double X
+        {
+            get; private set;
+        }
+        public double Y
+        {
+            get; private set;
+        }
+        public double Z
+        {
+            get; private set;
+        }
+
+    }
+}
