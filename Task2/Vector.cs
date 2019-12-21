0a91c63e42159d9ec61682e800a6b4aa693f2328
Павел Свинтицкий <pasha.svinticki@gmail.com>
2019-12-21 09:03:10 +0300

    Added operators.

--- Task2/Vector.cs
+++ Task2/Vector.cs
@@ -20,6 +20,53 @@ namespace Task2
         {
             get; private set;
         }
-
+        public Vector (double X, double Y, double Z)
+        {
+            this.X = X;
+            this.Y = Y;
+            this.Z = Z;
+        }
+        public static Vector operator +(Vector first, Vector second)
+        {
+            Vector result = new Vector(0, 0, 0);
+            result.X += first.X;
+            result.Y += first.Y;
+            result.Z += first.Z;
+            result.X += second.X;
+            result.Y += second.Y;
+            result.Z += second.Z;
+            return result;
+        }
+        public static Vector operator -(Vector first, Vector second)
+        {
+            Vector result = new Vector(0, 0, 0);
+            result.X += first.X;
+            result.Y += first.Y;
+            result.Z += first.Z;
+            result.X -= second.X;
+            result.Y -= second.Y;
+            result.Z -= second.Z;
+            return result;
+        }
+        public static Vector operator *(Vector vector, int number)
+        {
+            Vector result = new Vector(0, 0, 0);
+            result.X += vector.X * number;
+            result.Y += vector.Y * number;
+            result.Z += vector.Z * number;
+            return result;
+        }
+        public static Vector operator *(Vector first, Vector second)
+        {
+            Vector result = new Vector(0, 0, 0);
+            result.X = first.Y * second.Z - first.Z * second.Y;
+            result.Y = first.Z * second.X - first.X * second.Z;
+            result.Z = first.X * second.Y - first.Y * second.X;
+            return result;
+        }
+        public static double operator ^(Vector first, Vector second)
+        {
+            return first.X * second.X + first.Y * second.Y + first.Z * second.Z;
+        }
     }
 }
